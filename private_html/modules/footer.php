2ebaca1094e74b408572fb918404994f3af0143f
Oscar Freij <otg020313@gmail.com>
2022-09-24 23:11:10 +0200

    Add instagram link

--- private_html/modules/footer.php
+++ private_html/modules/footer.php
@@ -5,6 +5,9 @@
                 <span class="navbar-text col-12 col-md-4 text-md-start text-center">
                     Mobila solceller för att få energin du behöver, när du behöver den!
                 </span>
+                <a class="navbar-text col-12 col-md-4 text-md-center text-center" href="https://www.instagram.com/soly.uf/">
+                    <img src="static/icons/instagram.svg" alt="Instagram" height="24" width="24">
+                </a>
                 <span class="navbar-text col-12 col-md-4 text-md-end text-center">
                     Soly UF <a style="text-decoration:none;" href="?page=admin">&COPY;</a> 2022
                 </span>

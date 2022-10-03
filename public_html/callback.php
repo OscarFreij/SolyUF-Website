962cdf0799302e6172884d5d12f716d360e56526
Oscar Freij <otg020313@gmail.com>
2022-10-03 20:50:13 +0200

    Add functions to callback.php
    Added functions to callback.php
    Segrigated functions that require authentication and
    functions that do not require authentication.

--- public_html/callback.php
+++ public_html/callback.php
@@ -9,15 +9,60 @@ if (!isset($container))
 }
 
 
-// Callback functions that require client to be authenticated.
-if (isset($_SERVER['PHP_AUTH_USER']))
-{
 
+if (isset($_SERVER['PHP_AUTH_USER'])) // Callback functions that require client to be authenticated.
+{
+    switch ($_GET['action']) {
+        case 'addProduct':
+            http_response_code(501);
+            break;
+
+        case 'editProduct':
+            http_response_code(501);
+            break;
+
+        case 'removeProduct':
+            http_response_code(501);
+            break;
+
+        case 'toggleOrderSent':
+            http_response_code(501);
+            break;
+        
+        case 'toggleOrderPaid':
+            http_response_code(501);
+            break;
+        default:
+            http_response_code(400);
+            break;
+    }
 }
-
-// Callback functions that require no authentication.
-
-if (isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['message']))
+else // Callback functions that require no authentication.
 {
-    $container->functions()->sendFormEmail(array("email"=>$_POST['email'], "phone"=>$_POST['phone'], "msg"=>$_POST['message']));
+    switch ($_GET['action']) {
+        case 'contactForm':
+            if (isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['message']))
+            {
+                http_response_code(202);
+                $container->functions()->sendFormEmail(array("email"=>$_POST['email'], "phone"=>$_POST['phone'], "msg"=>$_POST['message']));
+            }
+            else
+            {
+                http_response_code(400);
+                return;
+            }
+            break;
+        case 'addToCart':
+            http_response_code(501);
+            break;
+        case 'removeFromCart':
+            http_response_code(501);
+            break;
+        case 'placeOrder':
+            http_response_code(501);
+            break;
+        default:
+            http_response_code(400);
+            break;
+    }
 }

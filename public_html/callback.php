b52dce34a597cc04953dc53a6f717dded54690a1
Oscar Freij <otg020313@gmail.com>
2022-10-06 02:48:11 +0200

    fix bad req respons when signed in

--- public_html/callback.php
+++ public_html/callback.php
@@ -37,76 +37,74 @@ if (isset($_SERVER['PHP_AUTH_USER'])) // Callback functions that require client 
             break;
     }
 }
-else // Callback functions that require no authentication.
-{
-    switch ($_POST['action']) {
-        case 'contactForm':
-            if (isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['message']))
+// Callback functions that require no authentication.
+switch ($_POST['action']) {
+    case 'contactForm':
+        if (isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['message']))
+        {
+            http_response_code(202);
+            $container->functions()->sendFormEmail(array("email"=>$_POST['email'], "phone"=>$_POST['phone'], "msg"=>$_POST['message']));
+        }
+        else
+        {
+            http_response_code(400);
+            return;
+        }
+        break;
+    case 'editCart':
+        if (isset($_POST['itemId']) && isset($_POST['itemCount']))
+        {
+            http_response_code(202);
+            if (isset($_SESSION['cart']))
             {
-                http_response_code(202);
-                $container->functions()->sendFormEmail(array("email"=>$_POST['email'], "phone"=>$_POST['phone'], "msg"=>$_POST['message']));
-            }
-            else
-            {
-                http_response_code(400);
-                return;
-            }
-            break;
-        case 'editCart':
-            if (isset($_POST['itemId']) && isset($_POST['itemCount']))
-            {
-                http_response_code(202);
-                if (isset($_SESSION['cart']))
-                {
-                    $itemIsNew = true;
-                    for ($i=0; $i < count($_SESSION['cart']); $i++) { 
-                        if ($_SESSION['cart'][$i]['id'] == $_POST['itemId'])
-                        {
-                            $_SESSION['cart'][$i]['count'] = $_POST['itemCount'];
-                            $itemIsNew = false;
-                            break;
-                        }
-                        else
-                        {
-                            continue;
-                        }
-                    }
-
-                    if ($itemIsNew)
+                $itemIsNew = true;
+                for ($i=0; $i < count($_SESSION['cart']); $i++) { 
+                    if ($_SESSION['cart'][$i]['id'] == $_POST['itemId'])
                     {
-                        array_push($_SESSION['cart'], array("id"=>$_POST['itemId'], "count"=>$_POST['itemCount']));
+                        $_SESSION['cart'][$i]['count'] = $_POST['itemCount'];
+                        $itemIsNew = false;
+                        break;
                     }
-                    
-
-                    
-                    $tempCart = Array();
-
-                    for ($i=0; $i < count($_SESSION['cart']); $i++) { 
-                        if ($_SESSION['cart'][$i]['count'] > 0)
-                        {
-                            array_push($tempCart, $_SESSION['cart'][$i]);
-                        }
+                    else
+                    {
+                        continue;
                     }
-
-                    $_SESSION['cart'] = $tempCart;
                 }
-                else
+
+                if ($itemIsNew)
                 {
-                    $_SESSION['cart'] = Array();
                     array_push($_SESSION['cart'], array("id"=>$_POST['itemId'], "count"=>$_POST['itemCount']));
                 }
+                
+
+                
+                $tempCart = Array();
+
+                for ($i=0; $i < count($_SESSION['cart']); $i++) { 
+                    if ($_SESSION['cart'][$i]['count'] > 0)
+                    {
+                        array_push($tempCart, $_SESSION['cart'][$i]);
+                    }
+                }
+
+                $_SESSION['cart'] = $tempCart;
             }
             else
             {
-                http_response_code(400);
-                return;
+                $_SESSION['cart'] = Array();
+                array_push($_SESSION['cart'], array("id"=>$_POST['itemId'], "count"=>$_POST['itemCount']));
             }
-            break;
-        case 'placeOrder':
-            http_response_code(501);
-            break;
-        default:
+        }
+        else
+        {
             http_response_code(400);
-            break;
-    }
+            return;
+        }
+        break;
+    case 'placeOrder':
+        http_response_code(501);
+        break;
+    default:
+        http_response_code(400);
+        break;
 }

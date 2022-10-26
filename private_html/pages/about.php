ecc19fca2a51bf7bcb234a128c56816a61549fac
Oscar Freij <otg020313@gmail.com>
2022-10-26 17:38:46 +0200

    add text to about.php

--- private_html/pages/about.php
+++ private_html/pages/about.php
@@ -9,7 +9,7 @@
                     Hur det började
                 </p>
                 <p class="fs-5 text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem tenetur explicabo aspernatur ea itaque consequatur cupiditate deserunt suscipit corporis sint blanditiis nisi vitae autem recusandae laudantium tempore, velit inventore omnis.
+                    Elpriserna hade mer än tredubblats från 2021-2022 och tanken att använda förnybar energi till vardags verkade nödvändig. Att ha en solcell som kan ta energin från solen i en brukbar storlek verkade vara en lösning på problemet.
                 </p>
             </div>
             <div class="mb-5 col-6">
@@ -17,7 +17,7 @@
                     Varför?
                 </p>
                 <p class="fs-5 text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem tenetur explicabo aspernatur ea itaque consequatur cupiditate deserunt suscipit corporis sint blanditiis nisi vitae autem recusandae laudantium tempore, velit inventore omnis.
+                    Miljön är i akut behov av hjälp mot klimatkrisen och privatpersoner betalar allt mer för elektricitet. Icke förnybar el och fossil bränsle är två metoder man idag använder allt för mycket av och något som behöver minskas på.
                 </p>
             </div>
             <div class="mb-5 col-12">
@@ -25,7 +25,7 @@
                     Resultatet
                 </p>
                 <p class="fs-5 text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem tenetur explicabo aspernatur ea itaque consequatur cupiditate deserunt suscipit corporis sint blanditiis nisi vitae autem recusandae laudantium tempore, velit inventore omnis.
+                En solcell som man enkelt kan fästa på sin tygkasse eller ryggsäck. Som är i en brukbar storlek och går att använda i solen under stora delar av året.
                 </p>
             </div>
         </div>

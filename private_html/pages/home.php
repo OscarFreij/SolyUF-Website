5eac094f927d9a5b310c60f006deb490009b1769
Oscar Freij <otg020313@gmail.com>
2022-10-27 10:40:13 +0200

    add images to home page

--- private_html/pages/home.php
+++ private_html/pages/home.php
@@ -4,4 +4,18 @@
             Soly - För att få energin du behöver, när du behöver den
         </p>
     </div>
+    <div class="row my-3">
+        <div class="container">
+            <div class="row">
+                <p class="fs-3 text-center text">
+                    Nedan ser du några bilder på våra produkter
+                </p>
+            </div>
+            <div class="row row-cols-1 row-cols-md-3 g-4 my-3">
+                <img class="homepage-image" src="static/images/products/2.jpeg" alt="">
+                <img class="homepage-image" src="static/images/products/1.jpeg" alt="">
+                <img class="homepage-image" src="static/images/products/4.jpeg" alt="">
+            </div>
+        </div>
+    </div>
 </div>

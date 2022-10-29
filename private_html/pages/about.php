351c732f76a273ee271a52b347064587e62b30c0
Oscar Freij <otg020313@gmail.com>
2022-10-29 21:36:25 +0200

    Fix about page text centering

--- private_html/pages/about.php
+++ private_html/pages/about.php
@@ -3,30 +3,32 @@
         <p class="fs-1 text-center text">
             Om oss
         </p>
-        <div class="row">
-            <div class="mb-5 col-6">
-                <p class="fs-3 text-center text">
-                    Hur det började
-                </p>
-                <p class="fs-5 text">
-                    Elpriserna hade mer än tredubblats från 2021-2022 och tanken att använda förnybar energi till vardags verkade nödvändig. Att ha en solcell som kan ta energin från solen i en brukbar storlek verkade vara en lösning på problemet.
-                </p>
-            </div>
-            <div class="mb-5 col-6">
-                <p class="fs-3 text-center text">
-                    Varför?
-                </p>
-                <p class="fs-5 text">
-                    Miljön är i akut behov av hjälp mot klimatkrisen och privatpersoner betalar allt mer för elektricitet. Icke förnybar el och fossil bränsle är två metoder man idag använder allt för mycket av och något som behöver minskas på.
-                </p>
-            </div>
-            <div class="mb-5 col-12">
-                <p class="fs-3 text-center text">
-                    Resultatet
-                </p>
-                <p class="fs-5 text">
-                En solcell som man enkelt kan fästa på sin tygkasse eller ryggsäck. Som är i en brukbar storlek och går att använda i solen under stora delar av året.
-                </p>
+        <div class="container">
+            <div class="row text-center">
+                <div class="mb-5 col-12 col-sm-6">
+                    <p class="fs-3 text-center text">
+                        Hur det började
+                    </p>
+                    <p class="fs-5 text">
+                        Elpriserna hade mer än tredubblats från 2021-2022 och tanken att använda förnybar energi till vardags verkade nödvändig. Att ha en solcell som kan ta energin från solen i en brukbar storlek verkade vara en lösning på problemet.
+                    </p>
+                </div>
+                <div class="mb-5 col-12 col-sm-6">
+                    <p class="fs-3 text-center text">
+                        Varför?
+                    </p>
+                    <p class="fs-5 text">
+                        Miljön är i akut behov av hjälp mot klimatkrisen och privatpersoner betalar allt mer för elektricitet. Icke förnybar el och fossil bränsle är två metoder man idag använder allt för mycket av och något som behöver minskas på.
+                    </p>
+                </div>
+                <div class="mb-5 col-12">
+                    <p class="fs-3 text-center text">
+                        Resultatet
+                    </p>
+                    <p class="fs-5 text">
+                        En solcell som man enkelt kan fästa på sin tygkasse eller ryggsäck. Som är i en brukbar storlek och går att använda i solen under stora delar av året.
+                    </p>
+                </div>
             </div>
         </div>
     </div>

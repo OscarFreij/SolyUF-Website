317977fc31dc634a2f42357a6e4d6e6ed0012f89
Oscar Freij <otg020313@gmail.com>
2022-11-04 21:16:18 +0100

    add order status icons in admin view

--- private_html/templates/content/admin/acordionItem.php
+++ private_html/templates/content/admin/acordionItem.php
@@ -1,20 +1,34 @@
 <?php
+$orderStatus = "";
 if ($order['paid']) {
     $paid = '<span class="text-success">Ja</span>';
+    $orderStatus = $orderStatus.'<i class="bi bi-credit-card text-success"></i>&nbsp;';
 } else {
     $paid = '<span class="text-danger">Nej</span>';
+    $orderStatus = $orderStatus.'<i class="bi bi-credit-card text-danger"></i>&nbsp;';
 }
 
 if ($order['sent']) {
     $sent = '<span class="text-success">Ja</span>';
+    $orderStatus = $orderStatus.'<i class="bi bi-send-check text-success"></i>';
 } else {
     $sent = '<span class="text-danger">Nej</span>';
+    $orderStatus = $orderStatus.'<i class="bi bi-send-dash text-danger"></i>';
 }
 ?>
 <div class="accordion-item">
     <h2 class="accordion-header" id="order<?= $order['id'] ?>">
         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#order<?= $order['id'] ?>collapse" aria-expanded="false" aria-controls="order<?= $order['id'] ?>collapse">
-            Order #<?= $order['id'] ?>
+            <span>
+                Order #<?= $order['id'] ?>
+            </span>
+            &nbsp;
+            &nbsp;
+            <span class="fs-4">
+            [
+            <?= $orderStatus ?>
+            ]
+            </span>
         </button>
     </h2>
     <div id="order<?= $order['id'] ?>collapse" class="accordion-collapse collapse" aria-labelledby="order<?= $order['id'] ?>" data-bs-parent="#orderAccordion">
